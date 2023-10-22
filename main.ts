3e8a787ace0ac6a83d943c7803d0b39aaa418c3e
maplesapling <148518615+maplesapling@users.noreply.github.com>
2023-10-22 00:15:58 -0400

    Add a command with a shortcut

--- main.ts
+++ main.ts
@@ -2,6 +2,22 @@ import { Plugin } from 'obsidian';
 
 export default class HelloWorldPlugin extends Plugin {
 	async onload() {
+		this.addCommand({
+			id: 'get-selected-text',
+			name: 'Get selected text',
+			callback: () => this.getSelectedText(),
+			hotkeys: [
+				{
+					modifiers: ["Mod", "Shift"],
+					key: "b"
+				}
+			]
+		});
+
 		this.addStatusBarItem().setText('Hello world');
+	}
+
+	async getSelectedText() {
+		this.addStatusBarItem().setText('Get selected text');
 	}
 }

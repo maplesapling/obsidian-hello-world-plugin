97f0a33ff4d6e2a6f71f8ab66eb54145ffd2db79
maplesapling <148518615+maplesapling@users.noreply.github.com>
2023-10-22 00:37:19 -0400

    Tidy: Tabs to spaces

--- main.ts
+++ main.ts
@@ -1,29 +1,29 @@
 import { MarkdownView, Plugin } from 'obsidian';
 
 export default class HelloWorldPlugin extends Plugin {
-	async onload() {
-		this.addCommand({
-			id: 'get-selected-text',
-			name: 'Get selected text',
-			callback: () => this.getSelectedText(),
-			hotkeys: [
-				{
-					modifiers: ["Mod", "Shift"],
-					key: "b"
-				}
-			]
-		});
+    async onload() {
+        this.addCommand({
+            id: 'get-selected-text',
+            name: 'Get selected text',
+            callback: () => this.getSelectedText(),
+            hotkeys: [
+                {
+                    modifiers: ["Mod", "Shift"],
+                    key: "b"
+                }
+            ]
+        });
 
-		console.log('Hello world');
-	}
+        console.log('Hello world');
+    }
 
-	async getSelectedText() {
-		let view = this.app.workspace.getActiveViewOfType(MarkdownView);
+    async getSelectedText() {
+        let view = this.app.workspace.getActiveViewOfType(MarkdownView);
 
-		let editor = view.editor;
+        let editor = view.editor;
 
-		let selection = editor.getSelection();
+        let selection = editor.getSelection();
 
-		console.log(`Selected text:\n${selection}`);
-	}
+        console.log(`Selected text:\n${selection}`);
+    }
 }
